bc50a445d7cd76e743136317cbd4c92420b35318
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-19 09:29:49 -0500

    configured app.js with middleware and port

--- app.js
+++ app.js
@@ -2,10 +2,37 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = ('./routes/');
 let app = express();
 
-// <Include the router index file>
-// <Setup your routes middleware>
-// <catch any undefined routes with a 404 middleware>
-// <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
-// middleware stack>
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// Setup your routes middleware
+app.use('/musichistory/api/', routes);
+
+
+// catch any undefined routes with a 404 middleware
+app.use((req, res, next) => {
+  // instantiate a new error and assign it
+  let err = new Error('404 Page Note Found');
+  // set status on our defined error
+  err.status = 404;
+  // call next to proceed down the chain with our error
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  console.log('ERROR :', err);
+  res.json({
+    message: err.message,
+    error: {}
+  })
+})
+
+// assign port to variable or listen on 3000 if there is not one established on envirnoment obj
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`The app is listening to your code on PORT: ${port}`);
+})
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 // <require express Router and require the songs.js router file>
-
-
+const { Router } = require('express');
+const router = Router();
 // this is an example of a root document. It returns a json representation of
 // all of our endpoints. (Just one endpoint exists in this tiny example app)
 router.get('/', function(req, res) {
@@ -11,3 +11,6 @@ router.get('/', function(req, res) {
     "songs": "http://localhost:3000/api/v1/songs",
   });
 });
+
+
+module.exports = router;
